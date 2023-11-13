02cff9ef00737e76f36cc9b19f96c1b193e77ce8
gonzaOlarte <olartegonzalo@gmail.com>
2023-11-13 17:31:44 -0300

    Cambio en la animacion (tamaño) y funcionalidad de enviar mensaje con Enter

--- Client/Client/ChatForm.cs
+++ Client/Client/ChatForm.cs
@@ -1,248 +1,273 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
 
-using System.Threading;
-using System.Net.Sockets;
-using System.IO;
-using Transitions;
+    using System.Threading;
+    using System.Net.Sockets;
+    using System.IO;
+    using Transitions;
 
-namespace Client
-{
-    public partial class ChatForm : Form
+    namespace Client
     {
-        // Declaración de variables y objetos estáticos utilizados en toda la clase.
-        static private NetworkStream stream;
-        static private StreamWriter streamw;
-        static private StreamReader streamr;
-        static private TcpClient client = new TcpClient();
-        static private string userName = "unknown";
-
-        // Delegado para actualizar la interfaz de usuario desde un hilo secundario.
-        private delegate void DaddItem(String s);
-
-        private static bool isDisconnecting = false;
-
-        // Método para agregar elementos a la lista (ListBox) en la interfaz de usuario.
-        private void AddItem(String s)
+        public partial class ChatForm : Form
         {
-            listBox1.Items.Add(s);
-        }
+            // Declaración de variables y objetos estáticos utilizados en toda la clase.
+            static private NetworkStream stream;
+            static private StreamWriter streamw;
+            static private StreamReader streamr;
+            static private TcpClient client = new TcpClient();
+            static private string userName = "unknown";
+            private bool isFormExpanded = false;  // Indicador del estado del formulario (expandido o no)
 
-        // Constructor de la clase `ChatForm`.
-        public ChatForm()
-        {
-            InitializeComponent();
-            this.Text = "Chat";
-        }
+            // Delegado para actualizar la interfaz de usuario desde un hilo secundario.
+            private delegate void DaddItem(String s);
 
-        // Método para escuchar los mensajes del servidor en un hilo secundario.
-        public void Listen()
-        {
-            while (client.Connected)
+            private static bool isDisconnecting = false;
+
+            // Método para agregar elementos a la lista (ListBox) en la interfaz de usuario.
+            private void AddItem(String s)
+            {
+                listBox1.Items.Add(s);
+            }
+
+            // Constructor de la clase `ChatForm`.
+            public ChatForm()
+            {
+                InitializeComponent();
+                this.Text = "Chat";
+            }
+
+            // Método para escuchar los mensajes del servidor en un hilo secundario.
+            public void Listen()
+            {
+                while (client.Connected)
+                {
+                    try
+                    {
+                        // Invoca el método `AddItem` en el hilo principal para actualizar la lista.
+                        this.Invoke(new DaddItem(AddItem), streamr.ReadLine());
+                    }
+                    catch (IOException)
+                    {
+                        // Ignora la excepción cuando se está desconectando intencionalmente.
+                        if (!isDisconnecting)
+                        {
+                            MessageBox.Show("Se ha desconectado correctamente");
+                            Application.Exit();
+                        }
+                    }
+                    catch
+                    {
+                        if (!isDisconnecting)
+                        {
+                            MessageBox.Show("No se pudo conectar al servidor");
+                            Application.Exit();
+                        }
+                    }
+                }
+            }
+
+            // Método para cargar el historial del chat.
+            private void LoadChatHistory()
+            {
+                if (File.Exists("chat_history.txt"))
+                {
+                    string[] chatHistory = File.ReadAllLines("chat_history.txt");
+                    foreach (string message in chatHistory)
+                    {
+                        AddItem(message);
+                    }
+                }
+            }
+
+            // Método para eliminar el historial del chat.
+            private void DeleteChatHistory()
+            {
+
+                DialogResult dialogResult = MessageBox.Show("¿Desea eliminar el historial?", "Historial", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    streamw.WriteLine("/deletehistory");
+                    streamw.Flush();
+                    listBox1.Items.Clear();
+                    MessageBox.Show("Historial eliminado");
+                }
+
+            }
+
+            // Método para conectar al servidor.
+            void Connect()
             {
                 try
                 {
-                    // Invoca el método `AddItem` en el hilo principal para actualizar la lista.
-                    this.Invoke(new DaddItem(AddItem), streamr.ReadLine());
-                }
-                catch (IOException)
-                {
-                    // Ignora la excepción cuando se está desconectando intencionalmente.
-                    if (!isDisconnecting)
+                    // Intenta conectarse al servidor en la dirección IP "127.0.0.1" y puerto 8000.
+                    client.Connect("127.0.0.1", 8000);
+
+                    // Llama al método para cargar el historial del chat
+                    LoadChatHistory();
+
+                    if (client.Connected)
                     {
-                        MessageBox.Show("Se ha desconectado correctamente");
-                        Application.Exit();
+                        // Si la conexión es exitosa, crea un hilo para escuchar los mensajes del servidor.
+
+                        // Obtiene el flujo de red para la comunicación.
+                        stream = client.GetStream();
+                        streamw = new StreamWriter(stream);
+                        streamr = new StreamReader(stream);
+
+                        // Envía el nombre de usuario al servidor.
+                        streamw.WriteLine(userName);
+                        streamw.Flush();
+
+                        btnDisconnect.Enabled = true;
+
+                        // Inicia el hilo de escucha.
+                        Thread t = new Thread(Listen);
+                        t.Start();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Servidor no disponible");
                     }
                 }
-                catch
-                {
-                    if (!isDisconnecting)
-                    {
-                        MessageBox.Show("No se pudo conectar al servidor");
-                        Application.Exit();
-                    }
-                }
-            }
-        }
-
-        // Método para cargar el historial del chat.
-        private void LoadChatHistory()
-        {
-            if (File.Exists("chat_history.txt"))
-            {
-                string[] chatHistory = File.ReadAllLines("chat_history.txt");
-                foreach (string message in chatHistory)
-                {
-                    AddItem(message);
-                }
-            }
-        }
-
-        // Método para eliminar el historial del chat.
-        private void DeleteChatHistory()
-        {
-
-            DialogResult dialogResult = MessageBox.Show("¿Desea eliminar el historial?", "Historial", MessageBoxButtons.YesNo);
-            if (dialogResult == DialogResult.Yes)
-            {
-                streamw.WriteLine("/deletehistory");
-                streamw.Flush();
-                listBox1.Items.Clear();
-                MessageBox.Show("Historial eliminado");
-            }
-
-        }
-
-        // Método para conectar al servidor.
-        void Connect()
-        {
-            try
-            {
-                // Intenta conectarse al servidor en la dirección IP "127.0.0.1" y puerto 8000.
-                client.Connect("127.0.0.1", 8000);
-
-                // Llama al método para cargar el historial del chat
-                LoadChatHistory();
-
-                if (client.Connected)
-                {
-                    // Si la conexión es exitosa, crea un hilo para escuchar los mensajes del servidor.
-
-                    // Obtiene el flujo de red para la comunicación.
-                    stream = client.GetStream();
-                    streamw = new StreamWriter(stream);
-                    streamr = new StreamReader(stream);
-
-                    // Envía el nombre de usuario al servidor.
-                    streamw.WriteLine(userName);
-                    streamw.Flush();
-
-                    btnDisconnect.Enabled = true;
-
-                    // Inicia el hilo de escucha.
-                    Thread t = new Thread(Listen);
-                    t.Start();
-
-                }
-                else
+                catch (Exception ex)
                 {
                     MessageBox.Show("Servidor no disponible");
+                    Application.Exit();
                 }
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Servidor no disponible");
-                Application.Exit();
-            }
-        }
 
-        // Método para desconectar del servidor cerrando los recursos asociados.
-        private void Disconnect()
-        {
-            try
+            // Método para desconectar del servidor cerrando los recursos asociados.
+            private void Disconnect()
             {
-                if (client != null && client.Connected)
+                try
                 {
-                    
-                    DialogResult dialogResult = MessageBox.Show("¿Desea desconectarse?", "Desconectar", MessageBoxButtons.YesNo);
-
-                    if (dialogResult == DialogResult.Yes)
+                    if (client != null && client.Connected)
                     {
-                        isDisconnecting = true;
+                    
+                        DialogResult dialogResult = MessageBox.Show("¿Desea desconectarse?", "Desconectar", MessageBoxButtons.YesNo);
 
-                        client.Close();
-                        streamw.Close();
-                        streamr.Close();
-                        stream.Close();
+                        if (dialogResult == DialogResult.Yes)
+                        {
+                            isDisconnecting = true;
 
-                        Console.WriteLine("Desconectado del servidor");
+                            client.Close();
+                            streamw.Close();
+                            streamr.Close();
+                            stream.Close();
+
+                            Console.WriteLine("Desconectado del servidor");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("No hay una conexión activa para desconectar.");
                     }
                 }
-                else
+                catch (Exception ex)
                 {
-                    MessageBox.Show("No hay una conexión activa para desconectar.");
+                    Console.WriteLine("Error al desconectar: " + ex.Message);
+                    MessageBox.Show("Error al desconectar: " + ex.Message);
+                }
+                finally
+                {
+                    isDisconnecting = false; // Restablece el indicador después de la desconexión.
                 }
             }
-            catch (Exception ex)
+
+
+            // Método que se ejecuta cuando se carga el formulario.
+            private void ChatForm_Load(object sender, EventArgs e)
             {
-                Console.WriteLine("Error al desconectar: " + ex.Message);
-                MessageBox.Show("Error al desconectar: " + ex.Message);
+                // Configura la ubicación de varios controles en posiciones fuera de la pantalla.
+                btnSend.Location = new Point(-500, 432);
+                txtMessage.Location = new Point(-500, 432);
+                listBox1.Location = new Point(-500, 25);
+                btnDeleteHistory.Location = new Point(-800, 482);
             }
-            finally
+
+            // Manejador de eventos para cuando se selecciona un elemento en la lista.
+            private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
             {
-                isDisconnecting = false; // Restablece el indicador después de la desconexión.
+                // No hay código específico en este manejador de eventos.
             }
-        }
 
+            // Manejador de eventos para el botón "Conectar".
+            private void btnConnect_Click(object sender, EventArgs e)
+            {
+                // Obtiene el nombre de usuario desde el cuadro de texto y llama al método `Connect` para establecer la conexión.
+                userName = txtUser.Text;
+                Connect();
 
-        // Método que se ejecuta cuando se carga el formulario.
-        private void ChatForm_Load(object sender, EventArgs e)
-        {
-            // Configura la ubicación de varios controles en posiciones fuera de la pantalla.
-            btnSend.Location = new Point(-500, 432);
-            txtMessage.Location = new Point(-500, 432);
-            listBox1.Location = new Point(-500, 25);
-            btnDeleteHistory.Location = new Point(-800, 482);
-        }
+                // Realiza una transición para mostrar controles en la interfaz de usuario.
+                Transition t = new Transition(new TransitionType_EaseInEaseOut(300));
+                this.Text = "Chat - "+userName;
+                t.add(lblUser, "Left", 700);
+                t.add(txtUser, "Left", 700);
+                t.add(btnConnect, "Left", 700);
+                t.add(listBox1, "Left", 25);
+                t.add(txtMessage, "Left", 25);
+                t.add(btnSend, "Left", 433);
+                t.add(btnDeleteHistory, "Left", 378);
 
-        // Manejador de eventos para cuando se selecciona un elemento en la lista.
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            // No hay código específico en este manejador de eventos.
-        }
+             // Cambia el tamaño del formulario según el estado actual
+            if (isFormExpanded)
+            {
+                this.Size = new Size(500, 300);  // Tamaño contraído
+            }
+            else
+            {
+                this.Size = new Size(550, 600);  // Tamaño expandido
+            }
 
-        // Manejador de eventos para el botón "Conectar".
-        private void btnConnect_Click(object sender, EventArgs e)
-        {
-            // Obtiene el nombre de usuario desde el cuadro de texto y llama al método `Connect` para establecer la conexión.
-            userName = txtUser.Text;
-            Connect();
+            isFormExpanded = !isFormExpanded;  // Invierte el indicador
 
-            // Realiza una transición para mostrar controles en la interfaz de usuario.
-            Transition t = new Transition(new TransitionType_EaseInEaseOut(300));
-            this.Text = "Chat - "+userName;
-            t.add(lblUser, "Left", 700);
-            t.add(txtUser, "Left", 700);
-            t.add(btnConnect, "Left", 700);
-            t.add(listBox1, "Left", 25);
-            t.add(txtMessage, "Left", 25);
-            t.add(btnSend, "Left", 433);
-            t.add(btnDeleteHistory, "Left", 378);
             t.run();
-        }
+            
 
-        // Manejador de eventos para el botón "Enviar".
-        private void btnSend_Click(object sender, EventArgs e)
-        {
-            // Lee el texto del cuadro de texto de mensaje y lo envía al servidor a través del flujo de escritura.
-            streamw.WriteLine(txtMessage.Text);
-            streamw.Flush();
-            // Borra el cuadro de texto de mensaje.
-            txtMessage.Clear();
-        }
+            }
 
-        private void btnDeleteHistory_Click(object sender, EventArgs e)
-        {
-            DeleteChatHistory();
-        }
+            // Manejador de eventos para el botón "Enviar".
+            private void btnSend_Click(object sender, EventArgs e)
+            {
+                // Lee el texto del cuadro de texto de mensaje y lo envía al servidor a través del flujo de escritura.
+                streamw.WriteLine(txtMessage.Text);
+                streamw.Flush();
+                // Borra el cuadro de texto de mensaje.
+                txtMessage.Clear();
+            }
 
-        private void btnDisconnect_Click(object sender, EventArgs e)
-        {
-            Disconnect();
-        }
+            private void btnDeleteHistory_Click(object sender, EventArgs e)
+            {
+                DeleteChatHistory();
+            }
 
-        //Evento para desconectar cuando el formulario se cierra.
-        private void ChatForm_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            Disconnect();
+            private void btnDisconnect_Click(object sender, EventArgs e)
+            {
+                Disconnect();
+            }
+
+            //Evento para desconectar cuando el formulario se cierra.
+            private void ChatForm_FormClosing(object sender, FormClosingEventArgs e)
+            {
+                Disconnect();
+            }
+
+            private void txtMessage_KeyPress(object sender, KeyPressEventArgs e)
+            {
+                if(e.KeyChar == Convert.ToChar(Keys.Enter))
+                {
+                    btnSend_Click(sender, e);
+                    e.Handled = true;
+                }
+            }
         }
     }
-}
 
